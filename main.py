1ea772da78b47c82ec2e38c28e6e984a642bc1db
Subhransu Das <subhransud525@gmail.com>
2026-06-09 00:42:23 +0530

    vercel deployment

--- main.py
+++ main.py
@@ -1736,4 +1736,4 @@ async def toggle_security_feature(body: dict):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8200, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
