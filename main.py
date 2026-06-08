982864e2dfdeff48441c5734bba2cd2d63490966
Subhransu Das <subhransud525@gmail.com>
2026-06-09 00:48:49 +0530

    vercel deployment

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:8200"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:8200","http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
